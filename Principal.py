d36f9d1e77625dd27c0d30038deb769aa95aa53d
GabrielRosa <g.rosaonofre@gmail.com>
2026-06-01 15:37:27 -0300

    Sem caracter especial

--- Principal.py
+++ Principal.py
@@ -2,20 +2,7 @@ from Menu import *
 from Listar import *
 from Atualizar import*
 
-def criar_conexao():
-    try:
-        conexao = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
-            password='Senac2026',
-            database='sistema_escolar'
-        )
-        return conexao
 
-    except Error as e:
-        print(f"Erro ao conectar ao MySQL: {e}")
-        return None
-    
 atualizar_aluno()
 listar_alunos()
 menu()
