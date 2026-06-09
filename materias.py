e2d0c444fd214b6dcc33d7030f5a8ef495236548
Adadazx <caioedavi2010@gmail.com>
2026-06-09 16:10:55 -0300

    Update materias.py

---
+++ materias.py
@@ -0,0 +1,14 @@
+
+def calcular_media_situacao(nota1, nota2):
+    media = (nota1 + nota2) / 2
+
+    if media >= 7:
+        situacao = "Aprovado"
+    elif media >= 5:
+        situacao = "Recuperação"
+    else:
+        situacao = "Reprovado"
+
+    return media, situacao                
+
+calcular_media_situacao()
