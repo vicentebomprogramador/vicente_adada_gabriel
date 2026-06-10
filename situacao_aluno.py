2765bdbae67c732b2ca8a1c6b53426a5d4e94267
Adadazx <caioedavi2010@gmail.com>
2026-06-10 16:21:25 -0300

    Create situacao_aluno.py

---
+++ situacao_aluno.py
@@ -0,0 +1,40 @@
+from cadastro_de_aluno_caio_gabrielRosa_vicente import *
+
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="Senac2026",
+    database="projeto_final"
+)
+
+cursor = conexao.cursor()
+
+matricula = input("Digite a matrícula do aluno: ")
+
+cursor.execute("""
+SELECT nota1, nota2
+FROM notas
+WHERE matricula = %s
+""", (matricula,))
+
+notas = cursor.fetchall()
+
+if len(notas) == 0:
+    print("Aluno não encontrado.")
+else:
+    for nota1, nota2 in notas:
+        media = (nota1 + nota2) / 2
+
+        print(f"\nNota 1: {nota1}")
+        print(f"Nota 2: {nota2}")
+        print(f"Média: {media:.1f}")
+
+        if media >= 7:
+            print("Situação: APROVADO")
+        else:
+            print("Situação: REPROVADO")
+
+cursor.close()
+conexao.close()
